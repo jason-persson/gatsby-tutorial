0fa83b02e5118886ed2c0385404e88b91cf4f77c
Jason Persson <jason.persson@gmail.com>
2022-01-13 14:16:03 +1100

    The rest of tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    siteMetadata: {
-      title: `Gatsby tutorial`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
+  siteMetadata: {
+    title: `Gatsby tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
